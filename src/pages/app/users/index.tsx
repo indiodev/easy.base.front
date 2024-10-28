b11310469cf3765fe45fdf4335dcf84bd52dbf6a
indiodev <jhollyfer.fr@gmail.com>
2024-10-28 00:41:16 -0500

    fix

--- src/pages/app/users/index.tsx
+++ src/pages/app/users/index.tsx
@@ -1,4 +1,3 @@
-import { Loading } from '@components/loading';
 import { Button } from '@components/ui/button';
 import { Input } from '@components/ui/input';
 import {
@@ -27,24 +26,6 @@ import { Table } from './table';
 
 export function Users(): React.ReactElement {
 	const { data: user_list, status: user_list_status } = useUserListQuery();
-
-	if (user_list_status === 'pending') {
-		return (
-			<Loading className="flex justify-center items-center h-full flex-1" />
-		);
-	}
-
-	if (user_list_status === 'error') {
-		return (
-			<section className="flex h-full flex-1 flex-col gap-4 w-full overflow-y-auto">
-				<div className="flex-1 w-full border border-indigo-100 bg-indigo-50/50 p-10 rounded-lg shadow-md flex flex-col gap-6">
-					<h1 className="text-3xl font-medium text-indigo-600">
-						Erro ao carregar dados
-					</h1>
-				</div>
-			</section>
-		);
-	}
 
 	return (
 		<div className="container max-w-full space-y-3 flex-1 w-full border border-indigo-100 bg-indigo-50/50 p-10 rounded-lg shadow-md flex flex-col gap-6">
@@ -83,10 +64,12 @@ export function Users(): React.ReactElement {
 				</div>
 			</section>
 
-			<Table
-				data={user_list}
-				labels={['Nome', 'Email']}
-			/>
+			{user_list_status === 'success' && (
+				<Table
+					data={user_list}
+					labels={['Nome', 'Email']}
+				/>
+			)}
 
 			<section className="inline-flex w-full justify-end">
 				<div className="inline-flex space-x-8 items-center">
