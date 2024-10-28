48d929c2677db853b99fb7630f700cd79cd04a18
indiodev <jhollyfer.fr@gmail.com>
2024-10-28 07:13:53 -0500

    fix: table main

--- src/pages/app/tables/index.tsx
+++ src/pages/app/tables/index.tsx
@@ -1,12 +1,25 @@
 /* eslint-disable @typescript-eslint/no-non-null-asserted-optional-chain */
 import { Loading } from '@components/loading';
 import { Button } from '@components/ui/button';
+import { Input } from '@components/ui/input';
 import {
 	Pagination,
 	PaginationContent,
 	PaginationItem,
 } from '@components/ui/pagination';
+import {
+	Select,
+	SelectContent,
+	SelectGroup,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from '@components/ui/select';
 import { Separator } from '@components/ui/separator';
+import { tanstack } from '@libs/tanstack';
+import { cn } from '@libs/utils';
+import { QUERY } from '@models/base.model';
+import { useUserTableLayoutMutation } from '@mutation/user/table-layout.mutation';
 import { useTableShowQuery } from '@query/table/show.query';
 import { useUserProfileQuery } from '@query/user/profile.query';
 import {
@@ -14,18 +27,22 @@ import {
 	ChevronRight,
 	ChevronsLeft,
 	ChevronsRight,
+	Filter as FilterLucideIcon,
+	LayoutDashboard,
+	LayoutList,
+	Search,
 } from 'lucide-react';
 import React from 'react';
 import { useParams, useSearchParams } from 'react-router-dom';
 import { Filter } from './filter';
-import { Header } from './header';
 import { Grid } from './layout/grid';
 import { List } from './layout/list';
 
 export function Tables(): React.ReactElement {
 	const params = useParams();
-
-	const [searchParams] = useSearchParams(new URLSearchParams(location?.search));
+	const [searchParams, setSearchParams] = useSearchParams(
+		new URLSearchParams(location?.search),
+	);
 
 	const { data: table, status: table_status } = useTableShowQuery({
 		id: params?.id || '',
@@ -36,36 +53,143 @@ export function Tables(): React.ReactElement {
 	const filterActive =
 		searchParams.has('filtered') && searchParams.get('filtered') !== 'false';
 
+	const {
+		mutateAsync: update_table_layout,
+		status: update_table_layout_status,
+	} = useUserTableLayoutMutation({
+		onSuccess() {
+			tanstack.refetchQueries({
+				queryKey: [QUERY.USER_PROFILE],
+			});
+		},
+		onError(error) {
+			console.error(error);
+		},
+	});
+
+	const [viewLayout, setViewLayout] = React.useState<'grid' | 'list'>('list');
+
+	React.useEffect(() => {
+		if (user_status === 'success' && params.id && user?.config?.table) {
+			setViewLayout(user?.config.table[params.id]?.layout);
+		}
+	}, [params, user, user_status]);
+
+	const isPendingTableOrUserData =
+		table_status === 'pending' || user_status === 'pending';
+
+	const isListLayout =
+		table_status === 'success' &&
+		user_status === 'success' &&
+		user?.config?.table?.[params?.id!]?.layout === 'list' &&
+		!isPendingTableOrUserData;
+
+	const isGridLayout =
+		table_status === 'success' &&
+		user_status === 'success' &&
+		user?.config?.table?.[params?.id!]?.layout === 'grid' &&
+		!isPendingTableOrUserData;
+
 	return (
 		<div className="flex-1 w-full border border-indigo-100 bg-indigo-50/50 p-10 rounded-lg shadow-md flex flex-col gap-6">
 			<h2 className="text-3xl font-medium text-indigo-600">{table?.title}</h2>
 
 			<Separator />
 
-			<Header />
+			<header className="w-full inline-flex items-center">
+				<section className="inline-flex w-full flex-1 space-x-3">
+					<div className="inline-flex items-center relative w-full">
+						<Search
+							className="w-5 h-5 absolute left-3 top-1/2 -translate-y-1/2 text-indigo-600"
+							strokeWidth={1.5}
+						/>
+						<Input
+							placeholder="Pesquise aqui"
+							className="pl-9 border border-indigo-200 placeholder:text-indigo-400 text-indigo-600 focus-visible:ring-indigo-600"
+						/>
+					</div>
 
-			{(table_status === 'pending' || user_status === 'pending') && (
+					<Button
+						className={cn(
+							'bg-transparent shadow-none text-indigo-600 hover:bg-indigo-50 border border-indigo-200 inline-flex gap-1',
+							filterActive && 'bg-indigo-600 text-white hover:bg-indigo-600',
+						)}
+						onClick={() => {
+							if (filterActive) {
+								searchParams.set('filtered', 'false');
+								setSearchParams(searchParams);
+								return;
+							}
+
+							setSearchParams((state) => {
+								state.set('filtered', 'true');
+								return state;
+							});
+						}}
+					>
+						<FilterLucideIcon className="w-5 h-5" />
+						<span>Filtro</span>
+					</Button>
+
+					<Button
+						className="bg-transparent hover:bg-transparent border shadow-none"
+						onClick={() => {
+							update_table_layout({
+								layout: viewLayout === 'list' ? 'grid' : 'list',
+								tableId: params.id,
+							});
+						}}
+					>
+						{viewLayout === 'list' && (
+							<LayoutDashboard className="w-5 h-5  text-gray-600" />
+						)}
+
+						{viewLayout === 'grid' && (
+							<LayoutList className="w-5 h-5  text-gray-600" />
+						)}
+					</Button>
+				</section>
+
+				<div className="flex-1 inline-flex space-x-2 items-center w-full justify-end">
+					<span>Resultados por página: </span>
+					<Select defaultValue="10">
+						<SelectTrigger className="w-[180px]">
+							<SelectValue placeholder="Selecione uma opção" />
+						</SelectTrigger>
+						<SelectContent>
+							<SelectGroup>
+								<SelectItem value="10">10</SelectItem>
+								<SelectItem value="20">20</SelectItem>
+								<SelectItem value="30">30</SelectItem>
+								<SelectItem value="40">40</SelectItem>
+								<SelectItem value="50">50</SelectItem>
+							</SelectGroup>
+						</SelectContent>
+					</Select>
+				</div>
+			</header>
+
+			{(isPendingTableOrUserData ||
+				update_table_layout_status === 'pending') && (
 				<Loading className="flex justify-center items-center h-screen flex-1" />
 			)}
 
 			<section className="inline-flex space-x-6">
-				{table_status === 'success' && filterActive && <Filter />}
-				{table_status === 'success' &&
-					user_status === 'success' &&
-					user?.config?.table?.[params?.id!]?.layout === 'list' && (
-						<List
-							columns={table?.columns}
-							rows={table.rows}
-						/>
-					)}
-				{table_status === 'success' &&
-					user_status === 'success' &&
-					user?.config?.table?.[params?.id!]?.layout === 'grid' && (
-						<Grid
-							columns={table?.columns}
-							rows={table?.rows}
-						/>
-					)}
+				{!(update_table_layout_status === 'pending') &&
+					!isPendingTableOrUserData &&
+					filterActive && <Filter />}
+				{!(update_table_layout_status === 'pending') && isListLayout && (
+					<List
+						columns={table?.columns}
+						rows={table.rows}
+					/>
+				)}
+				{!(update_table_layout_status === 'pending') && isGridLayout && (
+					<Grid
+						columns={table?.columns}
+						rows={table?.rows}
+					/>
+				)}
 			</section>
 
 			<section className="inline-flex w-full justify-end">
